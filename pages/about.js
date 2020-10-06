0e38abead34d8917171191380710b2bc09ec1e1e
Michael Gillett <michaelgillett19@gmail.com>
2020-10-06 14:08:10 -0700

    stuff

--- pages/about.js
+++ pages/about.js
@@ -1,14 +1,18 @@
-import Layout from "../components/Layout";
+import Layout from "./components/Layout";
 
-function AboutPage() {
-    return(
-        <Layout>
-            <div>
-                <h1>About</h1>
-                <p>I am very tired</p>
-            </div>
-        </Layout>
-    );
-}
+const AboutPage = (props) => (
+    <Layout>
+      <div className=".contianer-fluid">
+        <h1>About</h1>
+        <div className=".col-md-6">
+          <p>
+          My name is Michael and I am a graduate of Oregon State University with a Bachelor's Degree in Computer Science. I made this website
+          using Next.js, a framework for React focusing on server-side rendering. I also have experience in Python using Django, and C# using the .NET Core 
+          framework. 
+          </p>
+        </div>
+      </div>
+    </Layout>
+)
 
 export default AboutPage;
--- pages/index.js
+++ pages/index.js
@@ -1,13 +1,41 @@
-import Layout from "../components/Layout"
+import Layout from "./components/Layout";
 
-function HomePage() {
-    return(
-        <Layout>
-            <div>
-                <h1>Welcome</h1>
+import styles from '../styles/mainpage.module.css';
+
+const HomePage = (props) => (
+    <Layout>
+      <div className={styles.mainPage}>
+                <div className={styles.sidebar}>
+                    <img className={styles.img} src="profile.jpg" alt="Profile" />
+                    <p>
+                        Hello! My name is Michael Gillett. I am a recent graduate with a degree in Computer Science looking for a Software Developer job. I made this
+                        website using React.js and Typescript, with the hosting done through AWS Amplify.
+                        <br />
+                        <br />
+                        Twitter: <a href="https://www.twitter.com/MichaelDotPy">@MichaelDotPy</a>
+                        <br />
+                        Github: <a href="https://www.github.com/gillemic">github.com/gillemic</a>
+                        <br />
+                        Linkedin: <a href="https://www.linkedin.com/in/gillemic/">https://www.linkedin.com/in/gillemic/</a> 
+                    </p>
+                </div>
+                <div className={styles.mainPageBody}>
+                    <p>
+                        Just added a tetris page! It can be accessed via the Navigation bar at the 
+                        top of the page. 
+                    </p>
+                    <p>
+                        Finally got the Resume page working! It was tougher than I thought to
+                        render a PDF and after several failed ReactJS/npm packages, the JSX attribute
+                        &lt;iframe&gt; with the pdf as the source
+                    </p>
+                    <p>
+                        Todo list page is up! My first react project, made from create-react-app. Hope to
+                        add additional features such as list-saving or multiple lists!
+                    </p>
+                </div>
             </div>
-        </Layout>
-    );
-}
+    </Layout>
+)
 
 export default HomePage;
